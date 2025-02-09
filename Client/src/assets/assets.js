96fa240dea54b6bb2202c14f13495033a52c38d6
devops4sure <devops4sure@gmail.com>
2025-02-09 17:41:07 +0530

    changed plan

--- Client/src/assets/assets.js
+++ Client/src/assets/assets.js
@@ -82,19 +82,19 @@ export const testimonialsData = [
 export const plans = [
     {
       id: 'Basic',
-      price: 10,
+      price: 5,
       credits: 100,
       desc: 'Best for personal use.'
     },
     {
       id: 'Advanced',
-      price: 50,
+      price: 10,
       credits: 500,
       desc: 'Best for business use.'
     },
     {
       id: 'Business',
-      price: 250,
+      price: 150,
       credits: 5000,
       desc: 'Best for enterprise use.'
     },
